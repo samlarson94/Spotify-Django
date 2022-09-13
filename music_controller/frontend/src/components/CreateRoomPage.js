ea1b67834bc3b68c3f75e7fc90160ef0ecfe9798
samlarson94 <samlarson94@gmail.com>
2022-09-12 19:44:21 -0500

    Creating initial components. Adding dependencies.

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// NOTE: Refactor to Functional Componenets in future
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the room to Create a Room.</p>
+    }
+}
---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// NOTE: Refactor to Functional Componenets in future
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the home page.</p>
+    }
+}
